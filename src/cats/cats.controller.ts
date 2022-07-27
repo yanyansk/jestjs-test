e4f799f029a754ef3ac4ccdfdb09a7fc23c2fb7f
YasuakiSako <sakoyasuaki@gmail.com>
2022-07-27 23:27:38 +0900

    provider

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateCatDto } from './dto/cat.interface';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) { }
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+}
+
